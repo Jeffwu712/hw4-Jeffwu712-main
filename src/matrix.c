8d7b0a77ae0294a5ecfac3fd1d86016825fc281b
Jeffwu712 <124755490+Jeffwu712@users.noreply.github.com>
2023-10-22 02:58:32 -0400

    Update matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -2,8 +2,51 @@
 
 
 int SparseMatrix(int** M, int** S, int* D){
-    // TO BE IMPLEMENTED
-    abort();
+        int rows = D[0];
+    int cols = D[1];
+    int max_dimension = (rows > cols) ? rows : cols;
+    int non_zero_count = 0;
+
+    // Check if the matrix qualifies as a sparse matrix
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (*(*(M + i) + j) != 0) {
+                non_zero_count++;
+                if (non_zero_count > max_dimension) {
+                    // Matrix is not sparse
+                    return -1;
+                }
+            }
+        }
+    }
+
+    // Initialize the sparse matrix S using pointer dereferencing
+    *S = (int*)malloc(3 * max_dimension * sizeof(int));
+    int* sRow = *S;
+    int* sColumn = *S + max_dimension;
+    int* sValue = *S + 2 * max_dimension;
+
+    // Initialize S with zeros
+    for (int i = 0; i < max_dimension; i++) {
+        sRow[i] = 0;
+        sColumn[i] = 0;
+        sValue[i] = 0;
+    }
+
+    int col_index = 0;
+    // Fill the sparse matrix S with non-zero elements using pointer dereferencing
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (*(*(M + i) + j) != 0) {
+                sRow[col_index] = i;
+                sColumn[col_index] = j;
+                sValue[col_index] = *(*(M + i) + j);
+                col_index++;
+            }
+        }
+    }
+
+    return non_zero_count;
 }
 int Addition(int** M, int** N, int** A, int* D){
    // TO BE IMPLEMENTED
